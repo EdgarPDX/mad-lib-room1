f5aa9de15793593441e157eee59c8cdb245dd07c
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-06-29 11:30:31 -0700

    got input fields identified and validated

--- app.js
+++ app.js
@@ -7,6 +7,23 @@ console.log(boogerButton);
 
 boogerButton.addEventListener('click', () => {
     const noun1 = document.getElementById('noun1').value;
+    //console.log(noun1);
+    const noun1ans = document.getElementById('me')
+    console.log(noun1ans);
+
     const verb1 = document.getElementById('verb1').value;
+    //console.log(verb1);
+    const verb1ans = document.getElementById('wondering')
+    console.log(verb1ans)
+
     const pronouns = document.getElementById('pronouns').value; 
+    //console.log(pronouns);
+    const pronoun1ans = document.getElementById('everything');
+    console.log(pronoun1ans)
+
+    const location = document.getElementById('location').value;
+    //console.log(location);
+    const location1ans = document.getElementById('california')
+    console.log(location1ans)
+
 })
